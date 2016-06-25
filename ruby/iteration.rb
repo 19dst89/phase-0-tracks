bf1d9ca60ad6d1e6bebe33e31c7b2849dc582e90
Daniel Thompson <19dst89@gmail.com>
2016-06-25 16:20:00 -0400

    simple hash iteration with examples

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -17,3 +17,47 @@ puts ""
 #Hash
 
 pokemon_hash.each {|type, name| puts "#{name} is of the #{type} type"}
+
+#Array #1
+simple_array = ["a", "b", "C", "d", "E"]
+
+simple_array.delete_if { |letter| letter == letter.upcase }
+
+print simple_array
+
+#line break
+puts ""
+#Array #2
+
+
+simple_array_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+simple_array_2.keep_if { |number| number < 6 }
+
+print simple_array_2
+
+#Array  #3
+
+simple_array_3 = ["Orange Juice", "Dr Pepper", "Paper Towels", "Windex"]
+
+simple_array_3.select { |consonant| consonant == "a"}
+
+print simple_array_3
+
+#Hashes #1
+
+simple_hash_1 = { "a" => 100, "b" => 200, "c" => 300, "d" => 400 }
+
+simple_hash_1.reject { |key, value| key != "b" && key != "c" }
+
+#Hashes #2
+
+simple_hash_2 = { "a" => 100, "b" => 200, "c" => 300, "d" => 400 }
+
+simple_hash_2.keep_if { |letter, number| number < 250 }
+
+#Hashes #3
+
+simple_hash_3 = { "a" => 100, "b" => 200, "c" => 300, "d" => 400 }
+
+simple_hash_3.select { |letter, number| number >= 250 }
