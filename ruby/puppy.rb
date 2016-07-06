2d1f060ed92cf44e0d3d66bdbba0620869fcdbbf
Daniel Thompson <19dst89@gmail.com>
2016-07-06 13:46:55 -0400

    Define puppy class with characteristics in comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Woofious Canine
+
+# CHARACTERISTICS ----------------
+# Eye Color: Golden Brown with blue highlight
+# Fur Color: varies
+# Weight: 5 Lbs to 80 Lbs full grown
+# Name: varies
+# Tempermant: very friendly
+
+# BEHAVIOR -----------------------
+# Bark uncontrollably
+# Play Fetch
+# Roll over
+# PLay dead
