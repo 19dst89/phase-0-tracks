3636f631c7e9cf30a3b8abc5bb62549508d1fc06
Daniel Thompson <19dst89@gmail.com>
2016-07-08 10:49:55 -0400

    Working rspec test with modifications to todo_list_spec.rb

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,32 @@
+
+
+
+class TodoList
+
+	def initialize
+		@list = ["do the dishes", "mow the lawn"]
+	end
+
+	def get_items
+		@list
+	end
+
+	def add_item(added_item)
+		@list << added_item
+	end
+
+	def delete_item(deleted_item)
+		@list.delete(deleted_item)
+		@list
+	end
+
+	def get_item(integer)
+		@list.at(integer)
+	end
+
+end
+
+# p TodoList.new.get_items
+# p TodoList.new.add_item("mop")
+# p TodoList.new.delete_item("do the dishes")
+# p TodoList.new.get_item(0)
